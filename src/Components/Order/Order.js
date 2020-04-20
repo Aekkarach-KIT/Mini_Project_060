f3f07e6d3c36c0340a93eb3dbf862c9e8cfee8b8
Aekkarach-KIT <s6035512060@phuket.psu.ac.th>
2020-04-20 17:47:49 +0700

    ninth commit

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,32 +1,38 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import Button from "@material-ui/core/Button";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import { setCheckedOutItems } from "../../Redux/Actions";
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import Button from '@material-ui/core/Button'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import { setCheckedOutItems } from '../../Redux/Actions'
+import StripeCheckout from 'react-stripe-checkout'
 
 const mapStateToProps = state => {
   return {
     checkedOutItems: state.checkedOutItems
-  };
-};
+  }
+}
 
 // This component shows the items user checked out from the cart.
 class ConnectedOrder extends Component {
-  render() {
+  onToken = (token, addresses) => {
+    // TODO: Send the token information and any other
+    // relevant information to your payment process
+    // server, wait for the response, and update the UI
+    // accordingly. How this is done is up to you. Using
+    // XHR, fetch, or a GraphQL mutation is typical.
+  }
+  render () {
     let totalPrice = this.props.checkedOutItems.reduce((accumulator, item) => {
-      return accumulator + item.price * item.quantity;
-    }, 0);
+      return accumulator + item.price * item.quantity
+    }, 0)
 
     return (
       <div style={{ padding: 10 }}>
-        <div style={{ fontSize: 24, marginTop: 10 }}>
-          รายการสินค้า
-        </div>
+        <div style={{ fontSize: 24, marginTop: 10 }}>รายการสินค้า</div>
         <Table>
           <TableHead>
             <TableRow>
@@ -43,14 +49,14 @@ class ConnectedOrder extends Component {
                   <TableCell>{item.price}</TableCell>
                   <TableCell>{item.quantity}</TableCell>
                 </TableRow>
-              );
+              )
             })}
           </TableBody>
         </Table>
 
         <div
           style={{
-            color: "#504F5A",
+            color: '#504F5A',
             marginLeft: 5,
             marginTop: 50,
             fontSize: 22
@@ -59,20 +65,26 @@ class ConnectedOrder extends Component {
           ยอดรวม : {totalPrice} บาท
         </div>
         <Button
-          color="secondary"
-          variant="outlined"
+          color='secondary'
+          variant='outlined'
           disabled={totalPrice === 0}
           onClick={() => {
-            this.props.dispatch(setCheckedOutItems([]));
+            this.props.dispatch(setCheckedOutItems([]))
           }}
           style={{ margin: 5, marginTop: 30 }}
         >
           ละทิ้ง
         </Button>
+        <div className='payment'>
+          <StripeCheckout
+            stripeKey='pk_test_ez08krnCey8Ibzx5byDVbGNZ00R2hD7ps4'
+            token={this.onToken}
+          />
+        </div>
       </div>
-    );
+    )
   }
 }
-const Order = withRouter(connect(mapStateToProps)(ConnectedOrder));
+const Order = withRouter(connect(mapStateToProps)(ConnectedOrder))
 
-export default Order;
+export default Order
